27be10326ee16f679ff13df4fc1cf3022327c296
allenary <allenary@gmail.com>
2014-07-02 01:20:36 +0300

    CashOffice mock class added

---
+++ cinemaBom/src/main/java/com/sytoss/training/cinema/cinemaBom/Cashoffice.java
@@ -0,0 +1,24 @@
+package com.sytoss.training.cinema.cinemaBom;
+
+
+public class Cashoffice {
+	private int number;
+	private Cinema cinema;
+
+	public Cinema getCinema() {
+		return cinema;
+	}
+
+	public void setCinema(Cinema cinema) {
+		this.cinema = cinema;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+}
---
+++ cinemaBom/src/test/java/com/sytoss/training/cinema/cinemaBom/CashofficeTest.java
@@ -0,0 +1,14 @@
+package com.sytoss.training.cinema.cinemaBom;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CashofficeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
