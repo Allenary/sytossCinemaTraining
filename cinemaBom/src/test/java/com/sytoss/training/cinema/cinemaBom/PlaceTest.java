ee242084e85483a77ae6e0bfb5add16198bca6e6
Allenary <kochetovanina@gmail.com>
2014-07-02 13:09:41 +0300

    Revert "update .gitignore file & fixe failed test"
    This reverts commit 68734aaa79bb70b84b43fa807747161af2f5a5bd.

--- cinemaBom/src/test/java/com/sytoss/training/cinema/cinemaBom/PlaceTest.java
+++ cinemaBom/src/test/java/com/sytoss/training/cinema/cinemaBom/PlaceTest.java
@@ -19,7 +19,7 @@ public class PlaceTest {
 		place.setStatus(null);
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void testSetStatusCorrect() {
 		PlacesStatuses status = PlacesStatuses.DISABLE;
 		place.setStatus(status);
