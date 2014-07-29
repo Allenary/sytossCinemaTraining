06c121ec82e54c5c3440a91bfc2c483903411cdc
Allenary <kochetovanina@gmail.com>
2014-07-29 18:12:24 +0300

    start with read rows method

--- cinemaBom/src/main/java/com/sytoss/training/cinema/DomainService/TicketService.java
+++ cinemaBom/src/main/java/com/sytoss/training/cinema/DomainService/TicketService.java
@@ -1,6 +1,23 @@
 package com.sytoss.training.cinema.DomainService;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sytoss.training.cinema.bom.Ticket;
+import com.sytoss.training.cinema.translator.TicketTranslator;
 
 public class TicketService {
+
+  private List<Ticket> read(List<String> csvStrings) throws ParseException {
+    List<Ticket> tickets = new ArrayList<Ticket>();
+    String[] ticketParameters;
+    for (String row : csvStrings) {
+      ticketParameters = new CsvParser().parse(row);
+      tickets.add(new TicketTranslator().fromDTO(ticketParameters));
+    }
+    return tickets;
+
+  }
 
 }
