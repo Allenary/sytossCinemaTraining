7b260daaf6334e5703a9e441d702c54108adf1ef
Allenary <kochetovanina@gmail.com>
2014-08-06 16:40:18 +0300

    UI extended to support sml merge

--- cinemaBom/src/main/java/com/sytoss/training/cinema/UI/CSVFileMerge.java
+++ cinemaBom/src/main/java/com/sytoss/training/cinema/UI/CSVFileMerge.java
@@ -1,5 +1,6 @@
 package com.sytoss.training.cinema.ui;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +8,9 @@ import com.sytoss.training.cinema.domainservice.TicketService;
 
 public class CSVFileMerge {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     if (args == null || args.length < 2) {
+      printHelp();
       throw new IllegalArgumentException("Minimum 2 file pathes needed");
     }
     List<String> inputFileNames = new ArrayList<String>();
@@ -16,6 +18,28 @@ public class CSVFileMerge {
       inputFileNames.add(args[i]);
     }
     String outputFileName = args[args.length - 1];
-    new TicketService().mergeCSV(inputFileNames, outputFileName);
+    if (isXML(outputFileName)) {
+      new TicketService().mergeCSVToXML(inputFileNames, outputFileName);
+    } else if (isCSV(outputFileName)) {
+      new TicketService().mergeCSV(inputFileNames, outputFileName);
+    } else {
+      printHelp();
+      throw new IllegalArgumentException("Extension should be .XML or .CSV");
+    }
   }
+
+  private static void printHelp() {
+    System.out.println("CSVFileMerge <source> [<source>] <destination> ");
+    System.out.println("<source> - input file name");
+    System.out.println("<destination> - output file name could be CSV or XML");
+  }
+
+  private static boolean isCSV(String outputFileName) {
+    return outputFileName.matches(".*(\\.csv|\\.CSV)");
+  }
+
+  private static boolean isXML(String outputFileName) {
+    return outputFileName.matches(".*(\\.xml|\\.XML)");
+  }
+
 }
--- cinemaBom/src/test/java/com/sytoss/training/cinema/UI/CSVFileMergeTest.java
+++ cinemaBom/src/test/java/com/sytoss/training/cinema/UI/CSVFileMergeTest.java
@@ -1,18 +1,64 @@
 package com.sytoss.training.cinema.ui;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import org.junit.Test;
 
-import com.sytoss.training.cinema.ui.CSVFileMerge;
+import com.sytoss.training.cinema.testutils.TestUtils;
 
 public class CSVFileMergeTest {
 
   @Test(expected = IllegalArgumentException.class)
-  public void ShouldRaiseExceptionWhenListIsBlank() {
+  public void ShouldRaiseExceptionWhenListIsBlank() throws IOException {
     new CSVFileMerge().main(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void ShouldRaiseExceptionWhenListContainsOnly1Param() {
+  public void ShouldRaiseExceptionWhenListContainsOnly1Param() throws IOException {
     new CSVFileMerge().main(new String[] {"filename"});
+  }
+
+  @Test
+  public void shouldMergeToCSV() throws URISyntaxException, IOException {
+    String folder = "/shouldMerge2files";
+    String[] fileNames = new String[] {
+      new File(getClass().getResource(folder + "/1ticket.csv").toURI()).getAbsolutePath(),
+      new File(getClass().getResource(folder + "/3tickets.csv").toURI()).getAbsolutePath(),
+      new File(getClass().getResource(folder + "/testRunResult.csv").toURI()).getAbsolutePath()};
+
+    new CSVFileMerge().main(fileNames);
+
+    TestUtils.checkFiles(new File(getClass().getResource(folder + "/Standard.csv").toURI()).getAbsolutePath(), new File(getClass()
+      .getResource(folder + "/testRunResult.csv")
+      .toURI()).getAbsolutePath());
+  }
+
+  @Test
+  public void shouldMergeToXML() throws URISyntaxException, IOException {
+    String folder = "/shouldMerge2files";
+    String[] fileNames = new String[] {
+      new File(getClass().getResource(folder + "/1ticket.csv").toURI()).getAbsolutePath(),
+      new File(getClass().getResource(folder + "/3tickets.csv").toURI()).getAbsolutePath(),
+      new File(getClass().getResource(folder + "/testRunResult.xml").toURI()).getAbsolutePath()};
+
+    new CSVFileMerge().main(fileNames);
+
+    TestUtils.checkFiles(new File(getClass().getResource(folder + "/Standard.xml").toURI()).getAbsolutePath(), new File(getClass()
+      .getResource(folder + "/testRunResult.xml")
+      .toURI()).getAbsolutePath());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldShowMessageIfInvalidOutputFileName() throws URISyntaxException, IOException {
+    String folder = "/shouldMerge2files";
+    String[] fileNames = new String[] {
+      new File(getClass().getResource(folder + "/1ticket.csv").toURI()).getAbsolutePath(),
+      new File(getClass().getResource(folder + "/3tickets.csv").toURI()).getAbsolutePath(),
+      "notCSVORXML.txt"};
+
+    new CSVFileMerge().main(fileNames);
+
   }
 }
